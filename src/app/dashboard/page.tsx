c51b308c5b60d8a861700643d128d860fbb6c719
virtualheet <heetvavadiya456@gmail.com>
2025-08-27 17:50:58 +0530

    Enhance Home component layout by refining element positioning and adjusting padding for improved visual hierarchy. Update RoundButton component to support additional styling options for better integration with the overall design.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full flex flex-col justify-center items-center h-[100vh] min-h-screen bg-[#FEFBEC]'>
+      
+    </div>
+  )
+}
+
+export default page
