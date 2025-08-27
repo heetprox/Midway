eea0b5f838aaf659b166ef1d644202ade0b257c9
virtualheet <heetvavadiya456@gmail.com>
2025-08-27 18:09:28 +0530

    Add USDC address constants and implement address retrieval functions for different chains in addressHelpers.ts

---
+++ src/context/constants.ts
@@ -0,0 +1,13 @@
+
+export const OptimismUSDC = "0x79460d7f39d0C2cf0BB730Ac1fb56540edDDF691";
+export const ModeUSDC = "0x7AeC83cEC21521Fc883D5C02816cBB09c52919a7";
+export const EthUSDC = '0x9AB7b3dEDcF9BC4a4829588887c0d07F7FA323A9';
+export const ZoraUSDC = "0x7AeC83cEC21521Fc883D5C02816cBB09c52919a7";
+
+export const OptimismCore = "0x687fd495E18aBF1DD4aF09a6864d08a21661DC7D";
+export const EthClient = "0xB250a4B80ABE8C191ACF2311ae8199bfB553081a";
+export const ZoraClient = "0x6fef553421d3f3330a892C66b1DD8684ebdeD770";
+export const ModeClient = "0x6fef553421d3f3330a892C66b1DD8684ebdeD770";
+
+
+// export const MidpayABI = require("../contracts/ERC20Boo.sol/BooToken.json").abi;
--- src/utils/addressHelpers.ts
+++ src/utils/addressHelpers.ts
@@ -1,6 +1,39 @@
+import { EthUSDC, ModeUSDC, OptimismUSDC, ZoraUSDC } from "@/context/constants";
 import {
     optimismSepolia,
     sepolia as ethSepolia,
     modeTestnet as modeSepolia,
     zoraSepolia,
+    baseGoerli,
   } from "wagmi/chains";
+
+
+  export function getUsdcAddress(chain: number | undefined): string {
+    switch (chain) {
+      case optimismSepolia.id:
+        return OptimismUSDC;
+      case ethSepolia.id:
+        return EthUSDC;
+      case zoraSepolia.id:
+        return ZoraUSDC;
+      case modeSepolia.id:
+        return ModeUSDC;
+      default:
+        return OptimismUSDC;
+    }
+  }
+  
+  export function getOmniPayAddress(chain: number | undefined): string {
+    switch (chain) {
+      case optimismGoerli.id:
+        return contracts.OmniPayCore;
+      case baseGoerli.id:
+        return contracts.BaseOmniPayClient;
+      case zoraTestnet.id:
+        return contracts.ZoraOmniPayClient;
+      case modeTestnet.id:
+        return contracts.ModeOmniPayClient;
+      default:
+        return contracts.OmniPayCore;
+    }
+  }
