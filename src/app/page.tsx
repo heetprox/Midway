54d492517713ea2d8bb9c3a8046127c6e9f2f142
virtualheet <heetvavadiya456@gmail.com>
2025-08-27 16:41:22 +0530

    Refactor global styles and enhance home page layout. Introduce box-sizing and reset styles in globals.css. Update Home component with new padding and additional RoundButton for GitHub link.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="w-full flex flex-col justify-center items-center h-full min-h-screen bg-[#FEFBEC]">
-    <RoundButton text1="Connect" text2="Wallet" link="/" />
-   </div>
+    <div className="w-full flex flex-col justify-center items-center h-[100vh] min-h-screen bg-[#FEFBEC]"
+      style={{
+        padding: "clamp(1.25rem, 2vw, 200rem)",
+      }}
+    >
+      <div className="flex flex-col justify-between w-full h-full">
+        <RoundButton text1="Connect" text2="Wallet" link="/" />
+        <div className="flex flex-col items-center w-full">
+          <div className="b-font leading-none"
+            style={{
+              fontSize: "clamp(1.25rem, 13vw, 200rem)",
+            }}
+          >Midway</div>
+        </div>
+
+        <RoundButton text1="GIVE IT ★" text2="GITHUB" link="/" />
+
+      </div>
+    </div>
   );
 }
